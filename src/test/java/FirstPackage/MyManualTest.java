c464e24228c179e7eefbca07cb8ec5f5d84d059a
Dinesh Pallagani <pallagani.dinesh@gmail.com>
2025-07-06 16:00:32 +0530

    Latest Plugin changes

---
+++ src/test/java/FirstPackage/MyManualTest.java
@@ -0,0 +1,11 @@
+package FirstPackage;
+
+import org.junit.jupiter.api.Test;
+
+public class MyManualTest {
+
+    @Test
+    public void testPrintSomething() {
+        System.out.println("Hello from GitHub Actions test!");
+    }
+}
