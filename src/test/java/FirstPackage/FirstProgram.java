2366da97aefcb21bbe8747b810213561c16ef873
Dinesh Pallagani <pallagani.dinesh@gmail.com>
2025-07-06 15:34:13 +0530

    With lastest code

--- src/test/java/FirstPackage/FirstProgram.java
+++ src/test/java/FirstPackage/FirstProgram.java
@@ -1,6 +1,7 @@
+package FirstPackage;
 
 public class FirstProgram {
 	public static void main(String[] args) {
-		System.out.println("I'm the First program");
+		System.out.println("PRINTING FIRST OUTPUT");
 	}
 }
